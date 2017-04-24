7fe34e7b7ff2e0fe948826ba535fccbdc1faef1e
tlplu <tlp@loveyou.lu>
2017-04-24 16:13:26 +0300

    Added color function

---
+++ ran/color.py
@@ -0,0 +1,13 @@
+def color(clr, txt):
+    """ Return colorful text. """
+    colors = {
+        'black': '\x1b[90m',
+        'red': '\x1b[91m',
+        'green': '\x1b[92m',
+        'yellow': '\x1b[93m',
+        'blue': '\x1b[94m',
+        'purple': '\x1b[95m',
+        'cyan': '\x1b[96m',
+    }
+
+    return  colors[clr] + txt + '\x1b[0m'
