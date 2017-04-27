7c503129d4c7af53b1fbce83851e83c55d9bc094
tlplu <tlp@loveyou.lu>
2017-04-27 10:12:16 +0300

    Added hlp function

--- ran/screen.py
+++ ran/screen.py
@@ -1,5 +1,5 @@
 from .utils import getxy
-from .color import color
+from .color import color, underline
 
 
 logo_str = r""" _ __ __ _ _ __
@@ -37,7 +37,28 @@ def stats(cmd):
 
 def message(cmd):
     """ Display message and prompt and return input. """
-    print('Enter help')
+    if cmd not in ['', 'h', 'help']:
+        print(color('red', 'Bad syntax, enter [h]elp'))
+    else:
+        print('Enter [h]elp')
+
     cmd = input('> ')
 
     return cmd
+
+
+def hlp():
+    """ Display help. """
+    x, y = getxy()
+
+    for i in range(y - 16):
+        print()
+
+    print('\t ' + underline(color('yellow', 'Help')))
+    print()
+    print('\t Commands:')
+    print('\t\t ' + color('yellow', 'h, help') + ' - display this help text')
+    print('\t\t ' + color('yellow', 'q, quit') + ' - exit ran')
+
+    for i in range(3):
+        print()
