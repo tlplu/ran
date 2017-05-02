365c2d5f505099da1ada6c0d48a38f7469eba6c7
tlplu <tlp@loveyou.lu>
2017-05-02 14:56:22 +0300

    Initial log function

--- ran/screen.py
+++ ran/screen.py
@@ -1,3 +1,5 @@
+import datetime
+
 from .utils import getxy
 from .color import color, underline
 
@@ -62,3 +64,92 @@ def hlp():
 
     for i in range(3):
         print()
+
+
+def log():
+    """ Log workout. """
+
+    def details(workout):
+        """ Display workouts details"""
+        for i in range(y - 22):
+            print()
+
+        print('\t ' + underline(color('green', 'Workout details')))
+        print()
+        print('\t\t ' + color('green', 'date: ') + workout['date'])
+        print('\t\t ' + color('green', 'run: '))
+        print(
+            '\t\t   ' + color('green', 'type:') +
+            '\t' + workout['run']['type'])
+        print(
+            '\t\t   ' + color('green', 'duration: ') +
+            '\t' + workout['run']['duration'])
+        print(
+            '\t\t   ' + color('green', 'distance: ') +
+            '\t' + workout['run']['distance'])
+        print('\t\t ' + color('green', 'strength: '))
+        print(
+            '\t\t   ' + color('green', 'pull-ups: ') +
+            '\t' + workout['strength']['pull-ups'])
+        print(
+            '\t\t   ' + color('green', 'push-ups: ') +
+            '\t' + workout['strength']['push-ups'])
+        print(
+            '\t\t   ' + color('green', 'abs: ') +
+            '\t' + workout['strength']['abs'])
+
+        for i in range(3):
+            print()
+
+    x, y = getxy()
+    workout = {
+        'date': '',
+        'run': {'type': '', 'duration': '', 'distance': ''},
+        'strength': {'pull-ups': '', 'push-ups': '', 'abs': ''}
+    }
+    cancel = False
+
+    details(workout)
+    print('Enter date in YYYY-mm-dd format (default today) or [c]ancel')
+
+    while not cancel:
+        date = input('> ')
+
+        if date in ['c', 'cancel']:
+            cancel = True
+        else:
+            if date == '':
+                date = datetime.date.today()
+                break
+            else:
+                try:
+                    date = datetime.datetime.strptime(date, '%Y-%m-%d')
+                    break
+                except ValueError:
+                    logo()
+                    details(workout)
+                    print('Bad format, enter date in YYYY-mm-dd format')
+
+    if not cancel:
+        workout['date'] = date.strftime('%Y-%m-%d')
+
+        logo()
+        details(workout)
+
+        print('Enter type of run (default base) or [c]ancel')
+
+        while not cancel:
+            run = input('> ')
+
+            if run in ['c', 'cancel']:
+                cancel = True
+            else:
+                if run in ['base', '']:
+                    workout['run']['type'] = 'base'
+                    logo()
+                    details(workout)
+                    break
+                else:
+                    logo()
+                    details(workout)
+                    print('Bad type, enter type of run or [c]ancel')
