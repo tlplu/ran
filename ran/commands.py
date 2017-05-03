65bd417de31dce7d300ad232e08320934f334b5a
tlplu <tlp@loveyou.lu>
2017-05-03 11:51:20 +0300

    Updated commands

--- ran/commands.py
+++ ran/commands.py
@@ -1,10 +1,13 @@
-from .screen import hlp, stats
+from .screen import hlp, stats, log
 
 
 def commands(cmd):
     """ Call the proper function. """
     if cmd in ['h', 'help']:
         hlp()
+
+    elif cmd in ['l', 'log']:
+        log()
 
     else:
         stats(cmd)
