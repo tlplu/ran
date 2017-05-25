3dfe193e5bb9339fe106094b11a1ce86993585bd
tlplu <tlp@loveyou.lu>
2017-05-25 16:11:45 +0300

    Added TestMessage class

--- tests/test_screen.py
+++ tests/test_screen.py
@@ -1,4 +1,5 @@
 from ran.screen import logo_lines
+from ran.screen import message
 
 
 def test_logo_lines():
@@ -9,3 +10,18 @@ def test_logo_lines():
         '|_|  \\__,_|_| |_|']
 
     assert logo_lines() == lg
+
+
+class TestMessage:
+
+    def test_message_with_valid_command(self, capsys):
+        message('')
+        out, err = capsys.readouterr()
+
+        assert out == 'Enter [h]elp\n'
+
+    def test_message_with_invalid_command(self, capsys):
+        message('none')
+        out, err = capsys.readouterr()
+
+        assert out == '\x1b[91mBad syntax, enter [h]elp\x1b[0m\n'
