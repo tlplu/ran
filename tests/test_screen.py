483b9b80b937caa734e79efe548c976f18c7fa2c
tlplu <tlp@loveyou.lu>
2017-05-25 18:10:02 +0300

    Added TestLogo class

--- tests/test_screen.py
+++ tests/test_screen.py
@@ -1,15 +1,35 @@
-from ran.screen import logo_lines
-from ran.screen import message
+import os
+
+from ran.screen import logo_lines, message, logo
 
 
-def test_logo_lines():
-    lg = [
-        ' _ __ __ _ _ __  ',
-        "| '__/ _` | '_ \\ ",
-        '| | | (_| | | | |',
-        '|_|  \\__,_|_| |_|']
+class TestLogo():
 
-    assert logo_lines() == lg
+    def test_logo_lines(self):
+        lg = [
+            ' _ __ __ _ _ __  ',
+            "| '__/ _` | '_ \\ ",
+            '| | | (_| | | | |',
+            '|_|  \\__,_|_| |_|']
+
+        assert logo_lines() == lg
+
+    def test_logo(self, monkeypatch, capsys):
+        def mockreturn(fd):
+            return (15, 42)
+
+        monkeypatch.setattr(os, 'get_terminal_size', mockreturn)
+        logo()
+
+        out, err = capsys.readouterr()
+
+        text = (
+            '\n\n\x1b[96m _ __ __ _ _ __  \x1b[0m\n' +
+            "\x1b[96m| '__/ _` | '_ \\ \x1b[0m\n" +
+            '\x1b[96m| | | (_| | | | |\x1b[0m\n' +
+            '\x1b[96m|_|  \\__,_|_| |_|\x1b[0m\n')
+
+        assert out == text
 
 
 class TestMessage:
