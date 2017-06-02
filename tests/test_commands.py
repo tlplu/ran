e0d870fa7e8457c3508bb4fe8eca1f0277e50ee0
tlplu <tlp@loveyou.lu>
2017-06-02 09:52:17 +0300

    Improved test_hlp

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -7,14 +7,14 @@ from ran.commands import hlp, commands, stats
 
 def test_hlp(monkeypatch, capsys):
     def mockreturn(fd):
-        return(42, 10)
+        return(42, 17)
 
     monkeypatch.setattr(os, 'get_terminal_size', mockreturn)
     hlp()
 
     out, err = capsys.readouterr()
     text = (
-        '\t \x1b[4;93mHelp\x1b[0m\n\n' +
+        '\n\t \x1b[4;93mHelp\x1b[0m\n\n' +
         '\t Commands:\n' +
         '\t\t \x1b[93mh, help\x1b[0m - display this help text\n' +
         '\t\t \x1b[93mq, quit\x1b[0m - exit ran\n\n\n\n')
